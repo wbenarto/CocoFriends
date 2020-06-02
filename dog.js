dd8438dbefe93f11db4f43fd1006ec928f7086e1
kallistahalim <kallistahalim@gmail.com>
2020-06-01 23:51:01 -0700

    i started again

--- dog.js
+++ dog.js
@@ -1,5 +1,3 @@
-// var axios = require("axios");
-
 
 $("#submit").on("click", function() {
     var typeOfBreed = $("#breed").val().trim();
@@ -19,28 +17,12 @@ $("#submit").on("click", function() {
             var image = $("<img>");
             var dogPhoto = response.message[random];
             image.attr("src", dogPhoto);
-            image.attr("height", 100);
+            image.attr("height", 250);
             image.addClass("image-class");
             imageDiv.append(image);
             $("#photos").html(imageDiv, "<br>");
-        } else if(err) {
-            $("#photos").html(reponse.message);
-        }
-            
+        }  
     })
 
     return false;
 });
-
-
-
-// axios.get('https://dog.ceo/api/breed/' + typeOfBreed + '/images')
-//   .then(function (response) {
-//     var random = Math.floor(Math.random() * response.data.message.length);
-//     console.log(response.data.message[random]);
-
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
